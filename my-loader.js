34b6f11e54ea8081156845bbadcfc52a5516058c
dell7510 <dangne23102003@gmail.com>
2026-02-25 17:28:05 +0700

    new2

--- my-loader.js
+++ my-loader.js
@@ -1,3 +1,20 @@
 (function() {
     alert("dangnd");
+
+    // Log product title (nên kiểm tra tồn tại để tránh lỗi)
+    const titleEl = document.querySelector(".product-title");
+    if (titleEl) {
+        console.log("Product Title:", titleEl.innerText);
+    }
+
+    setTimeout(function() {
+        const btn = document.getElementById("writeReviewButton");
+        if (btn) {
+            btn.click();
+            console.log("Đã click writeReviewButton");
+        } else {
+            console.log("Không tìm thấy writeReviewButton");
+        }
+    }, 2000);
+
 })();
