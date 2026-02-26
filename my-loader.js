d6d1e25e11e0a53b63a94f1e05e6a460a8b6eb6c
dell7510 <dangne23102003@gmail.com>
2026-02-26 15:04:18 +0700

    new15

--- my-loader.js
+++ my-loader.js
@@ -1,60 +1,3 @@
-// (function () {
-//     const domain = "https://dangno1.github.io/testAppio";
-//     const iframeSrc = `${domain}/importerUI.html`;
-//     const containerId = "Appio-Importer-container";
-//     const iframeId = "Appio-Importer-iframe";
-
-//     function buildContainer(id) {
-//         const container = document.createElement('div');
-//         container.style =
-//             'width: 100vw; height: 100vh; position: fixed; top: 0; left: 0; z-index: 10000; background-color: rgba(0, 0, 0, 0.4); display: flex; justify-content: center; align-items: center;';
-//         container.setAttribute('id', id);
-//         return container;
-//     }
-
-//     function buildImportIframe(id) {
-//         const token = window.LAI_IMPORT_TOKEN || "";
-//         const iframe = document.createElement('iframe');
-//         iframe.src = `${iframeSrc}?token=${token}`;
-//         iframe.style =
-//             'border: none; max-height: 90vh; width: 530px; height: 480px; background-color: #fff; border-radius: 12px; box-shadow: 0 10px 30px rgba(0, 0, 0, 0.2); overflow: hidden;';
-//         iframe.id = id;
-//         return iframe;
-//     }
-
-//     function handleMessage(event) {
-//         if (event.data && event.data.type === 'MODAL/CLOSE') {
-//             const container = document.getElementById(containerId);
-//             container?.remove();
-//             window.removeEventListener('message', handleMessage);
-//         }
-//     }
-
-//     function init() {
-//         if (document.getElementById(containerId)) {
-//             console.log('Appio is already open.');
-//             return;
-//         }
-
-//         const container = buildContainer(containerId);
-//         const iframe = buildImportIframe(iframeId);
-
-//         window.addEventListener('message', handleMessage);
-
-//         container.appendChild(iframe);
-//         document.body.appendChild(container);
-
-//         setTimeout(function () {
-//             const btn = document.querySelector('#writeReviewButton');
-//             if (btn) {
-//                 console.log("Tìm thấy nút Write a review");
-//             }
-//         }, 2000);
-//     }
-
-//     init();
-// })();
-
 (function () {
     const titleEl = document.querySelector(".product-info-title");
     const productTitle = titleEl ? titleEl.innerText.trim() : "Không tìm thấy Product Title";
@@ -86,7 +29,7 @@
         <h3>Product Title:</h3>
         <p>${productTitle}</p>
         <button id="closePopupBtn" style="margin-top:15px;padding:8px 12px;cursor:pointer;">
-            Đóng
+            Close
         </button>
     `;
 
